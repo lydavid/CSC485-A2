178c89d0f5ebd6d92617b065bda0a7af9697beb2
David Ly <d.ly@mail.utoronto.ca>
2017-10-25 02:32:41 -0400

    1a rules

--- onea.pl
+++ onea.pl
@@ -36,3 +36,51 @@ vp
 cat> v,
 cat> np_acc.
 
+pp
+===>
+cat> p,
+cat> np_acc.
+
+np_nom
+===>
+cat> np.
+
+np_nom
+===>
+cat> pro_nom.
+
+np_acc
+===>
+cat> np.
+
+np_acc
+===>
+cat> pro_acc.
+
+np
+===>
+cat> det,
+cat> n.
+
+np
+===>
+cat> det,
+cat> n,
+cat> pp.
+
+np
+===>
+cat> npl.
+
+np
+===>
+cat> npl,
+cat> pp.
+
+n
+===>
+cat> nsg.
+
+n
+===>
+cat> npl.
