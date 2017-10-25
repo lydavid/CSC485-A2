70ae06c94f547b645410282f22801477a0eb8668
David Ly <d.ly@mail.utoronto.ca>
2017-10-25 14:26:04 -0400

    properly labeled rules

--- onea.pl
+++ onea.pl
@@ -32,56 +32,68 @@ s
 cat> np_nom,
 cat> vp. 
 
+vp_rule rule
 vp
 ===>
 cat> v,
 cat> np_acc.
 
+pp_rule rule
 pp
 ===>
 cat> p,
 cat> np_acc.
 
+np_nom_rule rule
 np_nom
 ===>
 cat> np.
 
+np_nom_rule rule
 np_nom
 ===>
 cat> pro_nom.
 
+np_acc_rule rule
 np_acc
 ===>
 cat> np.
 
+np_acc_rule rule
 np_acc
 ===>
 cat> pro_acc.
 
+np_rule rule
 np
 ===>
 cat> det,
 cat> n.
 
+np_rule rule
 np
 ===>
 cat> det,
 cat> n,
 cat> pp.
 
+np_rule rule
 np
 ===>
 cat> npl.
 
+np_rule rule
 np
 ===>
 cat> npl,
 cat> pp.
 
+n_rule rule
 n
 ===>
 cat> nsg.
 
+n_rule rule
 n
 ===>
 cat> npl.
