b3d7aaa68a7f9ceb88b7d990171eb8b14090b5ad
David Ly <d.ly@mail.utoronto.ca>
2017-11-05 21:53:14 -0500

    added comments to relate to Grammar 1, changed to having np features?

--- oneb.pl
+++ oneb.pl
@@ -1,16 +1,34 @@
 % David Ly, lydavid1, 1001435501
 
 % declare the features
-index sub [] intro [c:case, n:number, t:type].
+%index sub [] intro [c:case, n:number, t:type].
+%    case sub [nom, acc]. % nominative, accusative
+%        nom sub [].
+%        acc sub [].
+%    number sub [sing, plural]. % singular, plural
+%        sing sub [].
+%        plural sub [].
+%    type sub [common, pronoun]. % common noun, pronoun
+%        common sub [].
+%        pronoun sub [].
+
+
+% declare features for np
+index_np sub [] intro [c:case,t:type].
     case sub [nom, acc]. % nominative, accusative
         nom sub [].
         acc sub [].
-    number sub [sing, plural]. % singular, plural
-        sing sub [].
-        plural sub [].
     type sub [common, pronoun]. % common noun, pronoun
         common sub [].
         pronoun sub [].
+
+% declare features for n
+index_n sub [] intro [n:number].
+    number sub [sing, plural]. % singular, plural
+        sing sub [].
+        plural sub [].
+
+% now, how to incorporate np features?
 
 % declare the parts of speech
 cat sub [s,np,vp,pp,p,det,v].
@@ -36,45 +54,49 @@ him ---> (noun, index:(c:acc,t:pronoun)). %n.
 with ---> p.
 
 % augment Grammar 2 with features so as to restrict it to the language of Grammar 1
-% do not change the grammar below
-% actually, will have to change the specification
-% but we won't be adding/removing any of these below, only modifying
 
-% S -> NP VP
+% Grammar 1: S -> NPnom VP
+% Grammar 2: S -> NP VP
 srule rule
 s
 ===>
 cat> (np,head:(index:(c:nom))), % restricts to only allowing nominative
 cat> (vp,obj:(head:(index:(c:acc)))).
 
-% VP -> V NP
+% Grammar 1: VP -> V NPacc
+% Grammar 2: VP -> V NP
 vp_rule rule
 (vp,obj:(head:(index:(c:acc))))
 ===>
 cat> v,
 cat> (np,head:(index:(c:acc))).
 
-% PP -> P NP
+% Grammar 1: PP -> P NPacc
+% Grammar 2: PP -> P NP
 pp_rule rule
 pp
 ===>
 cat> p,
-cat> (np,head:(index:(c:acc)).
+cat> np.
 
-% NP -> N
+% Grammar 1: NP -> Nsg PP | Npl PP
+% Grammar 2: NP -> N PP
 np_rule rule
 (np,head:(index:(n:plural; t:pronoun)))
 ===>
-cat> (noun,index:(n:plural; t:pronoun)). % this rule allows it to accept she, which is a problem
+cat> (noun,index:(n:plural; t:pronoun)),
+cat> pp.
 
-% NP -> Det N
+% Grammar 1: NP -> Det Nsg | Det Npl
+% Grammar 2: NP -> Det N
 np_rule rule
 (np,head:(index:(t:common))) % should accept plural as well
 ===>
 cat> det,
 cat> (noun,index:(t:common)).
 
-% NP -> Det N PP
+% Grammar 1: NP -> Det Nsg PP | Det Npl PP
+% Grammar 2: NP -> Det N PP
 np_rule rule
 (np,head:(index:(t:common)))
 ===>
@@ -82,9 +104,9 @@ cat> det,
 cat> (noun,index:(t:common)),
 cat> pp.
 
-% NP -> N PP
+% Grammar 1: NP -> Nsg | Npl
+% Grammar 2: NP -> N
 np_rule rule
 (np,head:(index:(n:plural; t:pronoun)))
 ===>
-cat> (noun,index:(n:plural; t:pronoun)),
-cat> pp.
+cat> (noun,index:(n:plural; t:pronoun)). % this rule allows it to accept she, which is a problem
