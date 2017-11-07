dd57c7dd0ab5367467772dafcb782bb0c084ddf1
David Ly <d.ly@mail.utoronto.ca>
2017-11-06 20:55:56 -0500

    fixed tabbing 2->4 spaces

--- twosub.pl
+++ twosub.pl
@@ -4,37 +4,38 @@
 bot sub [mood, tense, sem, cat, pos, verbal, nominal].
 
 	% parts of speech
-        pos sub [n,p,v,det,toinf].
+    pos sub [n,p,v,det,toinf].
 		toinf sub [].	% infinitival to
-                n sub [].
-                v sub [].
-                p sub [].
-                det sub [].
-	% phrasal categories
-	cat sub [vproj,np].
-		vproj sub [inf_clause,s,vp] intro [mood:mood].
+        n sub [].
+        v sub [].
+        p sub [].
+        det sub [].
+
+    % phrasal categories
+    cat sub [vproj,np].
+        vproj sub [inf_clause,s,vp] intro [mood:mood].
 			s intro [mood:indicative].
-                        inf_clause intro [mood:infinitive].
+            inf_clause intro [mood:infinitive].
 			vp intro [mood:indicative].
 		np sub [].
 
         verbal sub [v,vproj] intro [vsem:v_sem].
         nominal sub [n,np] intro [nsem:n_sem].
-	
+
 	% mood and tense for verbs
 	tense sub [past, present].
-		past sub [].	
+		past sub [].
 		present sub [].
         mood sub [indicative,infinitive].
-                indicative intro [tense:tense].
-                infinitive sub [].
+            indicative intro [tense:tense].
+            infinitive sub [].
 
 	% semantics for verbs and nouns
 	sem sub [v_sem, n_sem].
 
 		% semantics for verbs
 		v_sem sub [prefer, persuade, promise, expect, sleep]
-		      intro [].   % This should not be empty!  Fill in features for this and
+                intro [].   % This should not be empty!  Fill in features for this and
                                   %  the following subtypes:
 			prefer sub [].
 			persuade sub [].
