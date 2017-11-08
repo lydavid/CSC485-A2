5b046ffe1fde9920e9f39767d345ee24b4fd1ef3
David Ly <d.ly@mail.utoronto.ca>
2017-11-08 01:07:21 -0500

    retry

--- twob.pl
+++ twob.pl
@@ -55,13 +55,13 @@ bot sub [mood, tense, sem, cat, pos, verbal, nominal, role].
 
 		% semantics for verbs
 		v_sem sub [prefer, persuade, promise, expect, sleep]
-                intro [vtense:tense].   % This should not be empty!  Fill in features for this and
+                intro [vtense:tense, obj:role].   % This should not be empty!  Fill in features for this and
                                   %  the following subtypes:
 			prefer sub [].% intro [subj:role, obj:role].%[subj:role, obj:role]. %[preferrer:np, preferree:np]. % preferrer must be a noun phrase, preferree could be anything?
-			persuade sub [] intro [subj:role, obj:role].%[agent:role, beneficiary:role, theme:role].
+			persuade sub [] intro [subj:role].%[agent:role, beneficiary:role, theme:role].
 			promise sub [].% intro [subj:role, obj:role].%[agent:role, beneficiary:role, theme:role].
 			expect sub [].% intro [subj:role, obj:role].%[agent:role, theme:role].
-			sleep sub [] intro [obj:role]. %[experiencer:role]. % in the interrogative sample, these take on index (sing/plural, trd/fst, ...)
+			sleep sub [].% intro [obj:role]. %[experiencer:role]. % in the interrogative sample, these take on index (sing/plural, trd/fst, ...)
 
 		% semantics for nouns
 		n_sem sub [student, teacher].
@@ -72,10 +72,10 @@ bot sub [mood, tense, sem, cat, pos, verbal, nominal, role].
 the ---> det.
 student ---> (n, nsem:student).
 teacher ---> (n, nsem:teacher).
-preferred ---> (v, vsem:(vtense:past, subj:agent, obj:theme)).
+%preferred ---> (v, vsem:(vtense:past, subj:agent, obj:theme)).
 persuaded ---> (v, vsem:(vtense:past, subj:agent, obj:beneficiary)). % don't need to assign role to theme (which will be an inf_clause)
-promised ---> (v, vsem:(vtense:past, subj:agent, obj:beneficiary)).
-expected ---> (v, vsem:(vtense:past, subj:agent, obj:theme)).
+%promised ---> (v, vsem:(vtense:past, subj:agent, obj:beneficiary)).
+%expected ---> (v, vsem:(vtense:past, subj:agent, obj:theme)).
 to ---> toinf.
 sleep ---> (v, vsem:(vtense:present, obj:Role)). % when this =agent, that means the agent of preferred/... is its obj, when it's =beneficiary, that means the agent of preferred/... is its obj (if it has any)
 
