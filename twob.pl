776640ff82ad85033b80537ca34ff37106f435f3
David Ly <d.ly@mail.utoronto.ca>
2017-11-08 18:38:08 -0500

    trying with Obj var in VP -> V inf_clause rule

--- twob.pl
+++ twob.pl
@@ -9,7 +9,7 @@ bot sub [mood, tense, sem, cat, pos, verbal, nominal, role].
 		toinf sub [].	% infinitival to
         n sub [].       % noun
         v sub [].       % verb
-        p sub [].       % preposition? -> doesn't look like any in lexicon though...
+        p sub [].       % preposition?
         det sub [].     % determinant
 
     % phrasal categories
@@ -20,28 +20,16 @@ bot sub [mood, tense, sem, cat, pos, verbal, nominal, role].
 			vp intro [mood:indicative].
 		np sub [].
 
-        % notice that inf_clause has a var, put fragments like "to sleep" as mood:infinitve, and "the teacher to sleep" with no mood
-
-    verbal sub [v, vproj] intro [vsem:v_sem].    % verbal has subtype verb, vproj?, with var vsem that could take on values from v_sem
-    nominal sub [n, np] intro [nsem:n_sem].      % nominal has subtypes noun, noun phrase, with var nsem
+    verbal sub [v, vproj] intro [vsem:v_sem].
+    nominal sub [n, np] intro [nsem:n_sem].
 
 	% mood and tense for verbs
-	tense sub [past, present]. % tense has subtypes past and present -> we don't have to handle present tense (ex: the student expects the teacher to sleep)
+	tense sub [past, present].
 		past sub [].
 		present sub [].
     mood sub [indicative,infinitive].
-        indicative intro [tense:tense]. % indicative has var tense that can take on values from tense
+        indicative intro [tense:tense].
         infinitive sub [].
-
-    % sentences and verb phrases take on indicative mood -> which have tense and should match throughout
-        % The indicative mood is used to make factual statements, ask questions,
-        %   or express opinions as if they were facts. Any verb tense may be deployed in the indicative mood.
-
-    % infinitive clause takes on infinitive mood -> which don't have tense
-        % Verbs in the infinitive mood are used as parts of speech more than verbs. It expresses being or action.
-        %   I may go to the beach later.
-        %   They came to speak to me.
-        %   It's important to eat well.
 
     % The possible roles for our verbs' obj/subj
     % the hierarchy allows our grammar to accept a broader category of roles such as "theme" as the obj
@@ -66,18 +54,13 @@ bot sub [mood, tense, sem, cat, pos, verbal, nominal, role].
                 % subj: which role is the subject of the verb
                 % obj: which role is the object of the verb
                 % ref: which role of the verb will be referenced as the obj of a later verb (sleep)
-                intro [vtense:tense, subj:role, obj:role, ref:role].   % This should not be empty!  Fill in features for this and
-                                  %  the following subtypes:
-			prefer sub [].% intro [subj:role, obj:role].%[subj:role, obj:role]. %[preferrer:np, preferree:np]. % preferrer must be a noun phrase, preferree could be anything?
-			persuade sub [].% intro [persuader:role, persuadee:role].%[agent:role, beneficiary:role, theme:role].
-			promise sub [].% intro [subj:role, obj:role].%[agent:role, beneficiary:role, theme:role].
-			expect sub [].% intro [subj:role, obj:role].%[agent:role, theme:role].
-			sleep sub [].% intro [obj:role]. %[experiencer:role]. % in the interrogative sample, these take on index (sing/plural, trd/fst, ...)
+                intro [vtense:tense, subj:role, obj:role, ref:role].
 
-            % possible obj for sleep: persuadee, expectee, preferrer, promiser
-
-            % subj of persuade is persuader, obj is persuadee
-            % obj of sleep in such a sentence is the persuadee
+			prefer sub [].
+			persuade sub [].
+			promise sub [].
+			expect sub [].
+			sleep sub [].
 
 		% semantics for nouns
 		n_sem sub [student, teacher].
@@ -98,7 +81,7 @@ sleep ---> (v, vsem:(vtense:present, obj:Role, ref:expectee)). % when this =agen
 
 % S -> NP VP
 srule rule
-(s, vsem:(vtense:past, subj:Subj, obj:Obj, ref:Gap))%mood:(tense:past))
+(s, vsem:(vtense:past, subj:Subj, obj:Obj, ref:Gap))
 ===>
 cat> np,
 cat> (vp, vsem:(vtense:past, subj:Subj, obj:Obj, ref:Gap)).%mood:(tense:past)).
@@ -114,13 +97,13 @@ cat> np.
 
 
 % VP -> V inf_clause
-% "...'preferred/expected/promised' 'to sleep'" -> all confirmed grammatically correct on bb
-% just handle preferred/expected for now (both of which has theme as its obj)
+% "...'preferred/expected/' 'to sleep'" -> all confirmed grammatically correct on bb
+% (both of which has theme as its obj)
 % *"the student persuaded to sleep"
 vp_rule rule
 (vp, vsem:(vtense:Tense, subj:Subj, obj:theme, ref:Gap))%mood:(tense:Tense))
 ===>
-cat> (v, vsem:(vtense:Tense, subj:Subj, obj:theme, ref:Gap)),
+cat> (v, vsem:(vtense:Tense, subj:Subj, obj:Obj, ref:Gap)),
 cat> (inf_clause, vsem:(vtense:Tense, subj:Subj, obj:Obj, ref:Gap)).
 
 
